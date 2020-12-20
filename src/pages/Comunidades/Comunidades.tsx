05bf6806a5e0472723b127756da84e1622b8aa28
GTS Soluções Tecnológicas <gmsoft.informatica@gmail.com>
2020-12-20 11:48:15 -0300

    feat: adjust page community

--- src/pages/Comunidades/Comunidades.tsx
+++ src/pages/Comunidades/Comunidades.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, Image} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import Header from '../../components/Header/Index';
 import SearchBar from '../../components/SearchBar/Index';
-import {Title} from '../Cadastro/styles';
 
 // import { Container } from './styles';
 import {withFormik} from 'formik';
+import {RectButton} from 'react-native-gesture-handler';
 
 const Comunidades: React.FC = (props: any) => {
   const [bairro, setBairro] = useState('');
@@ -34,48 +34,58 @@ const Comunidades: React.FC = (props: any) => {
             onChangeText={(text) => props.setFieldValue('numero', text)}
           />
         </View>
-        <View style={{flexDirection:'row',alignItems:'center'}}>
-        <View>
-          <Text style={styles.label}>Bairro</Text>
-          <Picker
-            selectedValue={bairro}
-            style={{height: 50, width: 150,borderColor:'#305F95',borderWidth:5}}
-            onValueChange={(itemValue, itemIndex) => {
-              setBairro(itemValue);
-              props.setFieldValue('bairro', itemValue);
-            }}>
-            <Picker.Item label="Jabutiana" value="0" />
-            <Picker.Item label="Olaria" value="2" />
-          </Picker>
-        </View>
-        <View>
-          <Text style={styles.label}>Cidade</Text>
-          <Picker
-            selectedValue={cidade}
-            style={{height: 50, width: 150,borderColor:'#305F95',borderWidth:5}}
-            onValueChange={(itemValue, itemIndex) => {
-              setCidade(itemValue);
-              props.setFieldValue('cidade', itemValue);
-            }}>
-            <Picker.Item label="Aracaju" value="0" />
-            <Picker.Item label="São Paulo" value="1" />
-            <Picker.Item label="Goias" value="2" />
-          </Picker>
-        </View>
-        <View>
-          <Text style={styles.label}>Estado</Text>
-          <Picker
-            selectedValue={estado}
-            style={{height: 50, width: 150,borderColor:'#305F95',borderWidth:5}}
-            onValueChange={(itemValue, itemIndex) => {
-              setEstado(itemValue);
-              props.setFieldValue('estado', itemValue);
-            }}>
-            <Picker.Item label="Sergipe" value="0" />
-            <Picker.Item label="São Paulo" value="1" />
-            <Picker.Item label="Goias" value="2" />
-          </Picker>
-        </View>
+        <View
+          style={{flexDirection: 'row', alignItems: 'center', marginTop: 25}}>
+          <View style={{borderColor: '#305F95', borderWidth: 1, margin: 2}}>
+            <Text style={styles.label}>Bairro</Text>
+            <Picker
+              selectedValue={bairro}
+              style={{
+                height: 50,
+                width: 130,
+              }}
+              onValueChange={(itemValue, itemIndex) => {
+                setBairro(itemValue);
+                props.setFieldValue('bairro', itemValue);
+              }}>
+              <Picker.Item label="Jabutiana" value="0" />
+              <Picker.Item label="Olaria" value="2" />
+            </Picker>
+          </View>
+          <View style={{borderColor: '#305F95', borderWidth: 1, margin: 2}}>
+            <Text style={styles.label}>Cidade</Text>
+            <Picker
+              selectedValue={cidade}
+              style={{
+                height: 50,
+                width: 130,
+              }}
+              onValueChange={(itemValue, itemIndex) => {
+                setCidade(itemValue);
+                props.setFieldValue('cidade', itemValue);
+              }}>
+              <Picker.Item label="Aracaju" value="0" />
+              <Picker.Item label="São Paulo" value="1" />
+              <Picker.Item label="Goias" value="2" />
+            </Picker>
+          </View>
+          <View style={{borderColor: '#305F95', borderWidth: 1, margin: 2}}>
+            <Text style={styles.label}>Estado</Text>
+            <Picker
+              selectedValue={estado}
+              style={{
+                height: 50,
+                width: 130,
+              }}
+              onValueChange={(itemValue, itemIndex) => {
+                setEstado(itemValue);
+                props.setFieldValue('estado', itemValue);
+              }}>
+              <Picker.Item label="Sergipe" value="0" />
+              <Picker.Item label="São Paulo" value="1" />
+              <Picker.Item label="Goias" value="2" />
+            </Picker>
+          </View>
         </View>
         <View style={styles.textAreaContainer}>
           <Text style={styles.label}>Descrição</Text>
@@ -83,10 +93,53 @@ const Comunidades: React.FC = (props: any) => {
             style={styles.input}
             value={props.values.descricao}
             multiline={true}
-            numberOfLines={8}
+            numberOfLines={4}
             underlineColorAndroid="transparent"
             onChangeText={(text) => props.setFieldValue('descricao', text)}
           />
+        </View>
+        <View>
+          <Text>Anexar Imagens e Videos</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              height: 85,
+              marginTop: 10,
+              marginBottom: 10,
+            }}>
+            <Image source={require('../assets/images/image62.png')} />
+            <Image source={require('../assets/images/image63.png')} />
+            <Image source={require('../assets/images/image64.png')} />
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignContent: 'space-between',
+          }}>
+          <RectButton
+            style={[
+              styles.button,
+              {backgroundColor: '#FFAB45', marginRight: 30},
+            ]}
+            onPress={() => {}}
+
+            // onPress={props.handleSubmit}
+          >
+            <Text style={styles.buttonText}>Inserir Imagem</Text>
+          </RectButton>
+          <RectButton
+            style={[styles.button, {backgroundColor: '#FE6D6C'}]}
+            onPress={() => {}}
+
+            // onPress={props.handleSubmit}
+          >
+            <Text style={styles.buttonText}>Enviar</Text>
+          </RectButton>
         </View>
       </View>
     </>
@@ -146,19 +199,19 @@ const styles = StyleSheet.create({
   submit: {},
   button: {
     backgroundColor: '#305F95',
-    height: 60,
+    height: 35,
     flexDirection: 'row',
-    borderRadius: 10,
+    borderRadius: 20,
     overflow: 'hidden',
     alignItems: 'center',
-    marginTop: 20,
+    width: 130,
   },
 
   buttonText: {
     flex: 1,
     justifyContent: 'center',
     textAlign: 'center',
-    color: '#000',
+    color: '#fff',
     fontFamily: 'Roboto_500Medium',
     fontSize: 16,
   },
